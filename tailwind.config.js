8005c07c29a061c2d05dd987e7e2b09a2684bc0a
donfolksy <sunepafolksy@rocketmail.com>
2024-05-30 14:49:58 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,18 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+     colors:{
+      gray: "#a1a5a6"
+     }
     },
+    screens:{
+      xm:'',
+       sm:'350px',
+       md:'640px',
+       lg:'768px',
+       xl:'951px',
+    
+    }
   },
   plugins: [],
 };
